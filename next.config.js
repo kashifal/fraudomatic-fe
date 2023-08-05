744539087e1c4bc55f6ab784872fe7876d251aa5
Kashif Sulehria <captainkashi4@gmail.com>
2023-08-05 06:08:28 +0500

    eslint

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  eslint:false
+  eslint: {
+    ignoreDuringBuilds: true,
+},
 }
 
 module.exports = nextConfig
